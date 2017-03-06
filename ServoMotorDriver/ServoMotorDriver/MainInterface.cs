180c2af9fd1f9206bb50ff05dcbb94101a3464be
Alex <alex.longbottom@hotmail.com>
2017-03-06 13:48:29 +0800

    File renaming

--- ServoMotorDriver/ServoMotorDriver/MainInterface.cs
+++ ServoMotorDriver/ServoMotorDriver/MainInterface.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ServoMotorDriver
 {
-    public partial class Form1 : Form
+    public partial class MainInterface : Form
     {
-        public Form1()
+        public MainInterface()
         {
             InitializeComponent();
         }
