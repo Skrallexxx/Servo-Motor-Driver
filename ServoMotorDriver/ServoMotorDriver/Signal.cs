0d5014083f45b98db694b110e56b9b4197d580e9
Jalmin <jalmin@live.com.au>
2017-03-07 09:44:51 +0800

    Added graphing functions to maininterface. Added signal class.

---
+++ ServoMotorDriver/ServoMotorDriver/Signal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServoMotorDriver
+{
+    class Signal
+    {
+        public string Name;
+        public double Value;
+        public string Units;
+
+        public Signal()
+        {
+            Name = "Defaul";
+            Value = 0.0;
+            Units = "Default";
+        }
+    }
+}
